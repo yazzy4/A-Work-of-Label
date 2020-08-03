6f45192e43c082bcbe0a643621fca408f34c7245
yazzy4 <yazburrell@pursuit.org>
2020-08-03 15:48:21 -0400

    Fixed label constraints

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -48,8 +48,17 @@ class ViewController: UIViewController {
         view.addSubview(label3)
         view.addSubview(label4)
         view.addSubview(label5)
+        
+        let viewsDictionary = ["label1" : label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        
+        for label in viewsDictionary.keys {
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        }
+        
+        let metrics = ["labelHeight": 88]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight)]-[label3(labelHeight)]-[label4(labelHeight)]-[label5(labelHeight)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
     }
-
 
 }
 
